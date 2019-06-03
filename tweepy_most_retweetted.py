ac895b1b899f5d4c5ffd324f4c6d178f9f49cab0
brakes312t4 <mscmacher@gmail.com>
2019-06-03 19:23:11 +0200

    Returning list of top tweets

--- tweepy_most_retweetted.py
+++ tweepy_most_retweetted.py
@@ -5,7 +5,7 @@ from operator import itemgetter
 
 import twitter_credentials
 
-def get_most_retweetted(hashtag):
+def get_most_retweetted(hashtag, top_n: int):
 	auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
 	auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
 	api = tweepy.API(auth,wait_on_rate_limit=True)
@@ -16,14 +16,25 @@ def get_most_retweetted(hashtag):
 	list_tweets = []
 
 	for tweet in tweepy.Cursor(api.search,q=hashtag, lang="en", since=date).items(20):
-		tweet_obj = (tweet.id, tweet.text, tweet.retweet_count)
-		list_tweets.append(tweet_obj)
-		# print(str(tweet_obj) + "\n")
-	
-	return max(list_tweets, key = itemgetter(2))
+		tweet_obj = (tweet.text, tweet.retweet_count)
+		if tweet_obj not in list_tweets:
+			list_tweets.append(tweet_obj)
+			# print(str(tweet_obj) + "\n")
 
+	if not list_tweets:
+		return [('None', 'None')]
+
+	list_most_retweetted_tweets = []
+
+	for i in range(min(top_n, len(list_tweets))): # this avoids error on max at next line
+		top_tweet = max(list_tweets, key = itemgetter(1))
+		list_most_retweetted_tweets.append(top_tweet)
+		list_tweets.remove(top_tweet)
+
+	return list_most_retweetted_tweets
 
 if __name__ == "__main__":
 	# Ask the hashtag the user is interested in
 	hashtag = input("Hashtag: ")
-	print(get_most_retweetted(hashtag))
+	top_n = int(input("Number of tweets: "))
+	print(get_most_retweetted(hashtag, top_n))
